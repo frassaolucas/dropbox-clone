abb4b4aaac0886ff27dfc4de77482f80074878c9
Lucas Frassao <frassao.lucas@gmail.com>
2020-08-24 22:49:38 -0300

    finished creating sidebar

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -17,6 +17,10 @@ interface Props {
 const Section: React.FC<Props> = ({ variant, title, description }) => {
   const buttonVariant = Math.round(Math.random());
 
+  function handleToggle() {
+    if (window.toggleActiveMenu) window.toggleActiveMenu();
+  }
+
   return (
     <Container className={variant}>
       <HeaderWrapper>
@@ -26,7 +30,7 @@ const Section: React.FC<Props> = ({ variant, title, description }) => {
             <span>Dropbox</span>
           </h1>
 
-          <button>{buttonVariant === 0 ? 'Acessar' : 'Interagir'}</button>
+          <button onClick={handleToggle}>{buttonVariant === 0 ? 'Acessar' : 'Interagir'}</button>
         </Header>
       </HeaderWrapper>
 
